29ff3a5159c8d8a4a23bb4ad5d876ff3f04f2b1f
clayton j mosher <mosherclayton@gmail.com>
2019-02-04 06:06:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
-function happyHollidays()
-  
-function happyHollidaysTo(name){console.log(`Happy hollidays, ${name}!`)}
+function happyHolidays(){
+  return "Happy Holidays!"}
+function happyHolidaysTo(name){
+  return `HappyHolidays, ${name}!`
+}
 function happyHolidayTo(holiday, name){console.log(`Happy ${holiday}, ${name}!`)}
 function holidayCountdown(holiday, days){console.log(`It's ${days} days until ${holiday}!`)}
