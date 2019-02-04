41f259136658c89ab751aad6812e0da06b7ad9bc
clayton j mosher <mosherclayton@gmail.com>
2019-02-04 05:56:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
-function happyHollidays(){console.log("Happy hollidays!")
+function happyHollidays(){console.log("Happy hollidays!")}
 function happyHolidaysTo(name){console.log)(`Happy holidays, ${name}!`)}
-function happyHolidayTo()
+function happyHolidayTo(holiday, name) {console.log(`Happy ${holiday}, ${name}!`)}
+function happ
